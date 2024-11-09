3fcff84f99412f990edbbf4bb4e1379f5cc7b263
sameera-g-mathad <sameeragmathad@gmail.com>
2024-11-08 18:43:13 -0600

    added sift code

--- implementations.py
+++ implementations.py
@@ -10,6 +10,7 @@ class Image(ABC):
 
     def __init__(self):
         self.images = []
+        self.location = []
 
     @abstractmethod
     def read_images(self, filepath: str):
@@ -43,7 +44,6 @@ class Grayscale(Image):
         super().__init__()
 
     def read_images(self, filepath):
-        self.location = []
         try:
             os.chdir(filepath)
             for file in os.listdir(filepath):
@@ -61,7 +61,21 @@ class Grayscale(Image):
         for index, image in enumerate(self.images):
             self.images[index] = cv.resize(self.images[index], (size, size))  # type: ignore
 
+    def rotate(self):
+        rotations = [
+            cv.ROTATE_90_CLOCKWISE,
+            cv.ROTATE_90_COUNTERCLOCKWISE,
+            cv.ROTATE_180,
+        ]
+        for index, image in enumerate(self.images):
+            self.images[index] = cv.rotate(image, np.random.choice(rotations))
+
     def __repr__(self):
+        for i in range(2):
+            index = np.random.randint(0, len(self.images))
+            plt.imshow(self.images[index], cmap="gray")
+            plt.axis("off")
+            plt.show()
         return f"Grayscale - {len(self.images)} images"
 
 
@@ -75,10 +89,44 @@ class SimpleImageFactory:
                 return RGB()
 
 
-class HOG:
-    def __init__(self, imageObj) -> None:
+class Descriptors:
+    def __init__(self, imageObj: Image) -> None:
         self.images = imageObj.images
         self.location = imageObj.location
+
+    def normalize(self, histogram) -> list[float]:
+        return histogram / (np.sqrt(np.sum(np.pow(histogram, 2))) + 1e-6)
+
+    def compute_hist(self, gx, gy) -> list[float]:
+        total_angle = 180
+        magnitude = np.sqrt(gx**2 + gy**2)
+        direction = np.arctan2(gx, gy) * (180 / np.pi)
+        direction[direction < 0] += total_angle
+
+        num_bins = 9
+        histogram = [0] * num_bins
+        bin_width = total_angle // num_bins
+        for index in range(len(direction)):
+            bin_number = int(direction[index] / bin_width)
+            bin_number %= num_bins
+            histogram[bin_number] += magnitude[index]
+
+        return self.normalize(histogram)
+
+    @abstractmethod
+    def process(
+        self,
+        add_image_location: bool,
+        append_value: int,
+        to_append: bool,
+        block: int = 8,
+    ) -> list:
+        pass
+
+
+class HOG(Descriptors):
+    def __init__(self, imageObj: Image) -> None:
+        super().__init__(imageObj)
 
     def calc_gradients(self, image):
         grad_x = np.zeros(image.shape, dtype=np.float32)
@@ -114,24 +162,24 @@ class HOG:
                 grad_y[row, col] = up - down
         return grad_x, grad_y
 
-    def normalize(self, histogram):
-        return histogram / (np.sqrt(np.sum(np.pow(histogram, 2))) + 1e-6)
+    # def normalize(self, histogram):
+    #     return histogram / (np.sqrt(np.sum(np.pow(histogram, 2))) + 1e-6)
 
-    def compute_hist(self, gx, gy):
-        total_angle = 180
-        magnitude = np.sqrt(gx**2 + gy**2)
-        direction = np.arctan2(gx, gy) * (180 / np.pi)
-        direction[direction < 0] += total_angle
+    # def compute_hist(self, gx, gy):
+    #     total_angle = 180
+    #     magnitude = np.sqrt(gx**2 + gy**2)
+    #     direction = np.arctan2(gx, gy) * (180 / np.pi)
+    #     direction[direction < 0] += total_angle
 
-        num_bins = 9
-        histogram = [0] * num_bins
-        bin_width = total_angle // num_bins
-        for index in range(len(direction)):
-            bin_number = int(direction[index] / bin_width)
-            bin_number %= num_bins
-            histogram[bin_number] += magnitude[index]
+    #     num_bins = 9
+    #     histogram = [0] * num_bins
+    #     bin_width = total_angle // num_bins
+    #     for index in range(len(direction)):
+    #         bin_number = int(direction[index] / bin_width)
+    #         bin_number %= num_bins
+    #         histogram[bin_number] += magnitude[index]
 
-        return self.normalize(histogram)
+    #     return self.normalize(histogram)
 
     def process(
         self,
@@ -165,6 +213,100 @@ class HOG:
         return self.hog
 
 
+class SIFT(Descriptors):
+    def __init__(self, imageObj) -> None:
+        super().__init__(imageObj)
+
+    def calc_scales(
+        self,
+        image: np.ndarray,
+        num_scale=5,
+        std=1.0,
+        ksize: tuple[int, int] = (3, 3),
+        scale_factor=2,
+    ) -> list[np.ndarray]:
+        blurred_images = []
+        for i in range(num_scale):
+            blurred_image = cv.GaussianBlur(image, ksize, std)
+            blurred_images.append(blurred_image)
+            std *= scale_factor
+        return blurred_images
+
+    def detect_keypoints(self, blurred_images: list[np.ndarray], threshold=0.2):
+        keypoints = []
+        for index in range(1, len(blurred_images) - 1):
+            # blurred_images[index + 1] = blurred_images[index + 1].astype(float) / 255
+            # blurred_images[index - 1] = blurred_images[index - 1].astype(float) / 255
+            diff_of_grad_images = blurred_images[index + 1] - blurred_images[index - 1]
+            diff_of_grad = (diff_of_grad_images > threshold) | (
+                diff_of_grad_images < -threshold
+            )
+            for x in range(1, len(diff_of_grad_images)):
+                for y in range(1, len(diff_of_grad_images)):
+                    if diff_of_grad[x, y] == 1:
+                        keypoints.append((x, y, index))
+        return keypoints
+
+    def descriptors(
+        self, image, keypoints: list[tuple[int, int, int]], block, clip: int
+    ):
+        size = block // 2
+        _descriptors = []
+        for x, y, scale in keypoints:
+            window = image[abs(x - size) : x + size, abs(y - size) : y + size]
+            grad_x = np.gradient(window, axis=1)
+            grad_y = np.gradient(window, axis=0)
+            _descriptors.extend(self.compute_hist(np.ravel(grad_x), np.ravel(grad_y)))
+
+        if len(_descriptors) < clip:
+            _descriptors = np.pad(
+                _descriptors,
+                (0, clip - len(_descriptors)),
+                mode="constant",
+                constant_values=(0, 0),
+            ).tolist()
+        return _descriptors[:clip]
+
+    # def normalize(self, histogram) -> list[float]:
+    #     return histogram / (np.sqrt(np.sum(np.pow(histogram, 2))) + 1e-6)
+
+    # def compute_hist(self, gx, gy) -> list[float]:
+    #     total_angle = 180
+    #     magnitude = np.sqrt(gx**2 + gy**2)
+    #     direction = np.arctan2(gx, gy) * (180 / np.pi)
+    #     direction[direction < 0] += total_angle
+
+    #     num_bins = 9
+    #     histogram = [0] * num_bins
+    #     bin_width = total_angle // num_bins
+    #     for index in range(len(direction)):
+    #         bin_number = int(direction[index] / bin_width)
+    #         bin_number %= num_bins
+    #         histogram[bin_number] += magnitude[index]
+
+    #     return self.normalize(histogram)
+
+    def process(
+        self,
+        add_image_location: bool,
+        append_value: int,
+        to_append: bool,
+        block: int = 8,
+    ) -> list:
+        self.sift = []
+        for i, image in enumerate(self.images[:100]):
+            histogram = [self.location[i]] if add_image_location else []
+            # first calculate all the scales using gaussian blur that detects different images.
+            blurred_images = self.calc_scales(image)
+            keypoints = self.detect_keypoints(blurred_images)
+            descriptors = self.descriptors(image, keypoints, block, 1000)
+            histogram.extend(descriptors)
+            if to_append:
+                histogram.append(append_value)
+            self.sift.append(histogram)
+        return self.sift
+
+
 class ImagePacker:
     def __init__(self, data: list[Image], resize: int = 200) -> None:
         self.data = []
@@ -177,8 +319,8 @@ class ImagePacker:
             output.append(imageObj)
         return output
 
-    def image_to_hog(self, data: list[Image], **kwargs) -> list[float]:
-        hog_features = []
+    def image_to_descriptor(self, data: list[Image], cls, **kwargs) -> list[float]:
+        _features = []
         append_label = kwargs.get("append_label", [])
         block_size = kwargs.get("block_size", 8)
         add_image_location = kwargs.get("add_image_location", True)
@@ -189,11 +331,15 @@ class ImagePacker:
         for index, imageObj in enumerate(data):
             if to_append:
                 value = append_label[index]
-            hog = HOG(imageObj)
-            hog_features.extend(
-                hog.process(add_image_location, value, to_append, block_size)
+            descriptor = cls(imageObj)
+            _features.extend(
+                descriptor.process(add_image_location, value, to_append, block_size)
             )
-        return hog_features
+        return _features
+
+    def rotate_images(self):
+        for index, imageObj in enumerate(self.data):
+            imageObj.rotate()
 
     def to_hog(self, **kwargs) -> pd.DataFrame | list:
         """
@@ -202,10 +348,20 @@ class ImagePacker:
 
         :return: Returns list of HOG features of images.
         """
-        hog = self.image_to_hog(self.data, **kwargs)
+        hog = self.image_to_descriptor(self.data, HOG, **kwargs)
         return hog
 
-    def hog_to_df(self, data, shuffle=True, target_col_name="target"):
+    def to_sift(self, **kwargs) -> pd.DataFrame | list:
+        """
+        To compute Histogram of gradients of an image.
+        :param imageObj: The image object that contains all the images to be converted to HOG features.
+
+        :return: Returns list of HOG features of images.
+        """
+        sift = self.image_to_descriptor(self.data, SIFT, **kwargs)
+        return sift
+
+    def descriptor_to_df(self, data, shuffle=True, target_col_name="target"):
         df = pd.DataFrame(data)
         df = df.rename(columns={df.columns[-1]: target_col_name})
         if shuffle:
